4a32e37760f04cf92a6ff7a966aa87d0382f9788
Denis <kanukov.denis@gmail.com>
2022-05-27 23:41:47 +0300

    init VUE3

---
+++ src/views/mainView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import {} from 'ax'
+export default {
+  name: "mainView",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    async fetchBooks() {
+
+    }
+  },
+  mounted() {
+    this.fetchBooks();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
