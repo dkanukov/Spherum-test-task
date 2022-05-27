9f73d108b7abad547851b18bfe5205d8a6fedc8f
Denis <kanukov.denis@gmail.com>
2022-05-28 00:46:00 +0300

    create request to server

--- src/views/mainView.vue
+++ src/views/mainView.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-import {} from 'ax'
 export default {
   name: "mainView",
   data() {
@@ -15,7 +14,15 @@ export default {
   },
   methods: {
     async fetchBooks() {
-
+      const responce = await fetch("http://45.8.249.57/bookstore-api/books", {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: '{"filters": {}}'
+      });
+      const result = await responce.json();
+      console.log(result);
     }
   },
   mounted() {
